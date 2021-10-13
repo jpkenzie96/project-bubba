0e1dfb4e819737d88c3f598bdff72f6b58a4b798
Kamren Davis <kamdavis@umich.edu>
2021-10-13 07:55:25 -0400

    Completed talk mode.

--- ProjectBubba/TalkVC.swift
+++ ProjectBubba/TalkVC.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class TalkVC: UIViewController {
+    @IBOutlet weak var yesNoLabel: UIButton!
+    var noImage = UIImage(named:"No")
+    var yesImage = UIImage(named:"Yes")
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sensorInput(_ sender: Any) {
+        self.yesNoLabel.setImage(yesImage, for: .normal)
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: false)
+    }
+    
+    @objc func timerAction() {
+        print("Timer Fired")
+        self.yesNoLabel.setImage(noImage, for: .normal)
+    }
+    
 }
