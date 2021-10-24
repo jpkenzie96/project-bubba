2cfe0526855e5dbf8a79ff19a38e1a77268478a4
vrohmetr <vrohmetr@umich.edu>
2021-10-24 05:15:51 -0400

    Added startup and calibration static pages

---
+++ ProjectBubba/Calibration.swift
@@ -0,0 +1,8 @@
+//
+//  Calibration.swift
+//  ProjectBubba
+//
+//  Created by Administrator on 10/23/21.
+//
+
+import Foundation
---
+++ ProjectBubba/StartupVC.swift
@@ -0,0 +1,16 @@
+//
+//  StartupVC.swift
+//  ProjectBubba
+//
+//  Created by Vania Rohmetra on 10/23/21.
+//
+
+import UIKit
+
+class StartupVC: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+}
