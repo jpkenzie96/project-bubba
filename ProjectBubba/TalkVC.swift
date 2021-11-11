a64926fce6be2c359daa2612e0932c6a3a8f2c1c
SydneyMacDonald <smacd@umich.edu>
2021-11-10 23:33:14 -0500

    added voice synthesizer to talk mode

--- ProjectBubba/TalkVC.swift
+++ ProjectBubba/TalkVC.swift
@@ -6,31 +6,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TalkVC: UIViewController {
     @IBOutlet weak var yesNoLabel: UIButton!
-    var timer = Timer()
+	var timer = Timer()
+	var enable = false;
+	var yes = false;
     let textColorChoices = ["Black", "Gray", "White", "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink"]
     let backgroundColorChoices = ["Red", "Black", "Gray", "White", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink"]
+	let synthesizer = AVSpeechSynthesizer()
+	
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let row = UserDefaults.standard.integer(forKey: "backgroundPickerViewRow")
         view.backgroundColor = SystemColor(color: backgroundColorChoices[row])
+		//sensor.isEnabled = false;
+		
     }
-
-    @IBAction func sensorInput(_ sender: Any) {
-        self.yesNoLabel.configuration?.background.backgroundColor = UIColor.green
-        self.yesNoLabel.configuration?.title = "Yes"
-                 
-        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: false)
-    }
+	@IBAction func asked(sender: Any){
+		//sensor.isEnabled = false;
+		self.enable = true;
+		timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(timerNo), userInfo: nil, repeats: false)
+	}
+    @IBAction func sensorInput(_ sender: UIButton) {
+		if (self.enable){
+			self.yes = true;
+			self.yesNoLabel.configuration?.background.backgroundColor = UIColor.green
+			self.yesNoLabel.configuration?.title = "Yes"
+			let yesspeak = AVSpeechUtterance(string: "yes")
+			yesspeak.voice = AVSpeechSynthesisVoice(language: "en-US")
+			synthesizer.speak(yesspeak)
+			
+					 
+			//timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: false)
+		}
+        
+	}
     
-    @objc func timerAction() {
+    /*@objc func timerAction() {
         self.yesNoLabel.configuration?.background.backgroundColor = UIColor.red
         self.yesNoLabel.configuration?.title = "No"
         
-    }
-    
+    }*/
+	@objc func timerNo(){
+		if (!self.yes)
+		{
+			let nospeak = AVSpeechUtterance(string: "no")
+		nospeak.voice = AVSpeechSynthesisVoice(language: "en-US")
+			synthesizer.speak(nospeak)
+			
+		}
+		if (self.yes){
+			self.yesNoLabel.configuration?.background.backgroundColor = UIColor.red
+			self.yesNoLabel.configuration?.title = "No"
+		}
+		self.yes = false
+		self.enable = false
+	}
 }
